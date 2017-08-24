7f0e0f4726aa6ecddca8cd54f72c3fe57ae1e463
benwu <benwu9999@gmail.com>
2017-08-23 22:01:33 -0400

    remove crsf

--- LocationService/admin_site/settings.py
+++ LocationService/admin_site/settings.py
@@ -45,9 +45,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware', # for allowing CORS
+    'corsheaders.middleware.CorsMiddleware',  # for allowing CORS
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
