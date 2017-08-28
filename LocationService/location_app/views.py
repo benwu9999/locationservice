061c41627f885c577cd9602df31cc875eb414ab0
benwu9999 <benwu9999@gmail.com>
2017-08-28 17:34:37 +0000

    temp add dynamic query

--- LocationService/location_app/views.py
+++ LocationService/location_app/views.py
@@ -1,9 +1,10 @@
 import json
-
+import time
 import sys
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.db.models import Q
 
 from serializers import *
 
@@ -22,10 +23,23 @@ class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
 class LocationSearch(APIView):
     def get(self, request, format=None):
         try:
+            args = Q()
+            kwargs = {}
             if 'ids' in request.query_params:
-                serializer = LocationSerializer(Location.objects.filter(
-                    pk__in=request.query_params['ids'].split(',')), many=True)
-
-                return Response(serializer.data)
+                kwargs['pk__in'] = request.query_params['ids'].split(',')
+            if 'within' in request.query_params:
+                kwargs['created__gt'] = self.get_epoch(request.query_params['within'])
+            if 'has' in request.query_params:
+                args = args | Q(**{'name__icontains':request.query_params['has']})
+                args = args | Q(**{'state__icontains': request.query_params['has']})
+                args = args | Q(**{'country__icontains': request.query_params['has']})
+                args = args | Q(**{'street_address__icontains': request.query_params['has']})
+                args = args | Q(**{'city__icontains': request.query_params['has']})
+                args = args | Q(**{'zip_code__icontains': request.query_params['has']})
+            z = LocationSerializer(Location.objects.filter(*args, **kwargs), many=True)
+            return Response(z.data)
         except:
             return Response(sys.exc_info()[0])
+            
+    def get_epoch(self, epoch):
+        return int(time.time()) - epoch
