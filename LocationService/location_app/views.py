9af063eab2a60d4e61392ed11870eda71151f123
benwu <benwu9999@gmail.com>
2017-08-28 21:20:57 -0400

    fixed search

--- LocationService/location_app/views.py
+++ LocationService/location_app/views.py
@@ -1,6 +1,7 @@
-import json
 import time
 import sys
+
+import operator
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -20,26 +21,28 @@ class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
 
+
 class LocationSearch(APIView):
     def get(self, request, format=None):
         try:
-            args = Q()
-            kwargs = {}
+            qs = list()
             if 'ids' in request.query_params:
-                kwargs['pk__in'] = request.query_params['ids'].split(',')
+                qs.append(Q(pk__in=request.query_params['ids'].split(',')))
             if 'within' in request.query_params:
-                kwargs['created__gt'] = self.get_epoch(request.query_params['within'])
+                qs.append(Q(created__gt=self.get_epoch(request.query_params['within'])))
             if 'has' in request.query_params:
-                args = args | Q(**{'name__icontains':request.query_params['has']})
-                args = args | Q(**{'state__icontains': request.query_params['has']})
-                args = args | Q(**{'country__icontains': request.query_params['has']})
-                args = args | Q(**{'street_address__icontains': request.query_params['has']})
-                args = args | Q(**{'city__icontains': request.query_params['has']})
-                args = args | Q(**{'zip_code__icontains': request.query_params['has']})
-            z = LocationSerializer(Location.objects.filter(*args, **kwargs), many=True)
+                text_qs = list()
+                text_qs.append(Q(**{'name__icontains': request.query_params['has']}))
+                text_qs.append(Q(**{'state__icontains': request.query_params['has']}))
+                text_qs.append(Q(**{'country__icontains': request.query_params['has']}))
+                text_qs.append(Q(**{'street_address__icontains': request.query_params['has']}))
+                text_qs.append(Q(**{'city__icontains': request.query_params['has']}))
+                text_qs.append(Q(**{'zip_code__icontains': request.query_params['has']}))
+                qs.append(reduce(operator.or_, text_qs))
+            z = LocationSerializer(Location.objects.filter(reduce(operator.and_, qs)), many=True)
             return Response(z.data)
         except:
             return Response(sys.exc_info()[0])
-            
+
     def get_epoch(self, epoch):
         return int(time.time()) - epoch
